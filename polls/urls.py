43598752cc0d8a5dac2f0a5db2c3937b46d2eb8f
JJOJJORing <liverforest@gmail.com>
2019-11-12 16:07:14 +0900

    fixing views.py and applying generic view

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, detail_view, result_view, vote
+from .views import IndexView, DetailView, ResultView, vote
 
+
+# 뷰파일에 함수명은 바로 쓰지만 클래스를 사용한 제네릭뷰인 리스트뷰나 디테일뷰를 사용시 뒤에 as.view() 를사용한다
 urlpatterns = [
-    path('', index, name='index'),
-    path('detail/<int:request_id>', detail_view),
-    path('result/<int:request_id>', result_view),
+    path('', IndexView.as_view(), name='index'),
+    path('detail/<int:request_id>', DetailView.as_view()),
+    path('result/<int:request_id>', ResultView.as_view()),
     path('vote/<int:request_id>', vote)
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,45 +1,44 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
-def index(request):
-    output = ''
-    for question in Question.objects.order_by('-pub_date'):
-        output += '<a href="/polls/detail/' + str(question.id) + '">' + question.question_text + '</a><br>'
+# 제네릭 뷰인 list view 로 바꿈
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
-    return HttpResponse(output)
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-
-def detail_view(request, request_id):
-    # 페이지나 데이터가 있을때는 가져오고 없으면 404페이지를 띄운다
-    question = get_object_or_404(Question, pk=request_id)
-    choice_list = get_list_or_404(question.choice_set)
-
-    # form 태그를 쓸때는 csrf토큰을 사용해 보안을 막아야 한다.
-
-    output = ''
-    output += '<h1>' + question.question_text + '</h1>'
-    output += '<form action="/polls/vote/' + str(question.id) + '" method="post">'
-
-    for choice in choice_list:
-        output += '<input type="radio" name="choice" value="' + str(choice.id) + '"/> ' + choice.choice_text + '<br>'
-
-    output += '<input type="submit" value="Vote">'
-    output += '</form>'
-    return HttpResponse(output)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
 
 
-def result_view(request, request_id):
-    question = get_object_or_404(Question, pk=request_id)
-    choice_list = get_list_or_404(question.choice_set)
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    output = '<h1>' + question.question_text + '</h1>'
-    for choice in choice_list:
-        output += choice.choice_text + ' : ' + str(choice.votes) + '<br>'
 
-    return HttpResponse(output)
+class ResultView(DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+
+
+
+# def detail_view(request, request_id):
+#     # 페이지나 데이터가 있을때는 가져오고 없으면 404페이지를 띄운다
+#     question = get_object_or_404(Question, pk=request_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+#
+#
+# def result_view(request, request_id):
+#     question = get_object_or_404(Question, pk=request_id)
+#     return render(request, 'polls/result.html', {'question': question})
 
 
 def vote(request, request_id):
